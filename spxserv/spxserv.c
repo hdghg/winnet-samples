6993af78aaf453505362299df68320c1607efa72
Alexey Tokar <tokar.a.v@yandex.ru>
2021-11-04 17:32:46 +0300

    Initial commit. Add server stub

---
+++ spxserv/spxserv.c
@@ -0,0 +1,12 @@
+// spxserv.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main(int argc, char **argv)
+{
+    printf("Stub works. Press ENTER key...\n");
+    getchar();
+    return 0;
+}
